2e20d65c391fb18ed70758eb9374537b5207e17f
NormalPerson <153537220+DavidMarquesSousa@users.noreply.github.com>
2025-11-06 19:08:18 +0000

    passo 2 completo

---
+++ script.js
@@ -0,0 +1,26 @@
+function addVal(value) {
+
+    // This line finds YOUR input field using its specific ID "resultados"
+    let display = document.getElementById('input');
+
+    // This line adds the number (7) to whatever is already in the box
+    display.value += value;
+}
+
+function deleteAll() {
+    let display = document.getElementById('input');
+    display.value = '';
+}
+
+function calculate() {
+    let display = document.getElementById('input');
+    try {
+        if (display.value != '') {
+        display.value = eval(display.value);
+        }else{
+            alert("Please enter a valid expression to calculate.");
+        }
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
